10b4731b33a31155ecf2273c35dc30366e261c02
Yunsuding <steven@ihuizong.cn>
2018-12-10 11:08:22 +0800

    默认

---
+++ widget/script/js.js
@@ -0,0 +1,101 @@
+// 获取时间
+
+function getUrl(e){
+    let domain = 'https://demo.isaveu.cn';
+    switch(e){
+        case 'login':
+            return domain+'/human/api.login/index';  //  登录 
+        break;
+        case 'getting':
+            return domain+'/human/api.Usersetting/getting'; //  通知设置
+        break;
+        case 'edit':
+            return domain+'/human/api.Usersetting/edit'; //  通知设置保存
+        break;
+        case 'getMine':
+            return domain+'/human/api.User/getMine';   //  查看工作状态
+        break;
+        case 'editWorkStatus':
+            return domain+'/human/api.User/editWorkStatus';
+        break;
+        case 'admit':
+            return domain+'/human/api.servicecall/admit';
+        break;
+        case 'info':
+            return domain+'/human/api.servicecall/info';
+        break;
+        case 'call_deal':
+            return domain+'/human/api.servicecall/call_deal';
+        break;
+        case 'getUserInfo':
+            return domain+'/human/api.servicecall/getUserInfo';
+        break;
+        case 'bind':
+            return domain+'/human/api.index/bind';
+        break;
+        default :
+            return domain+'/human/api.watch/watch_Login_out';
+        break;
+    }
+}
+
+function getNowFormatDate() {
+    var date = new Date();
+    var seperator1 = "-";
+    var seperator2 = ":";
+    var month = date.getMonth() + 1;
+    var strDate = date.getDate();
+    if (month >= 1 && month <= 9) {
+        month = "0" + month;
+    }
+    if (strDate >= 0 && strDate <= 9) {
+        strDate = "0" + strDate;
+    }
+    var currentdate = date.getFullYear() + seperator1 + month + seperator1 + strDate
+            + " " + date.getHours() + seperator2 + date.getMinutes()
+            + seperator2 + date.getSeconds();
+    return currentdate;
+}
+
+function PrefixInteger(num, length) {
+ return (Array(length).join('0') + num).slice(-length);
+}
+
+function Clock() {
+    var date = new Date();
+    this.year = date.getFullYear();
+    this.month = date.getMonth() + 1;
+    this.date = date.getDate() < 10 ? "0" + date.getDate() : date.getDate();
+    this.day = new Array("星期日", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六")[date.getDay()];
+    this.hour = date.getHours() < 10 ? "0" + date.getHours() : date.getHours();
+    this.minute = date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes();
+    this.second = date.getSeconds() < 10 ? "0" + date.getSeconds() : date.getSeconds();
+    this.getDate = function(){
+        return this.date;
+    }
+    this.getDay = function(){
+        return this.day;
+    }
+    this.getHours = function(){
+        return this.hour;
+    }
+    this.getMinutes = function(){
+        return this.minute;
+    }
+    this.getSeconds = function(){
+        return this.second;
+    }
+    this.display = function(getMinutes,getHours,getday,getdate,getSecond) {
+        var clock = new Clock();
+        getMinutes.innerHTML = clock.getMinutes();
+        getHours.innerHTML = clock.getHours();
+        getday.innerHTML = clock.getDay();
+        getdate.innerHTML = clock.getDate();
+        if(this.second%2 == 0){
+            getSecond.style.color = 'white';
+        }else{
+            getSecond.style.color = 'red';
+        }
+        window.setTimeout(function() {clock.display(getMinutes,getHours,getday,getdate,getSecond);}, 1000);
+    }; 
+}
